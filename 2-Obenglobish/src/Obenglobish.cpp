28470d44a5d2d70a3586ff9570e76460ab8e57ff
cs-1-vprihogenko <vprihogenko@gmail.com>
2015-08-23 02:34:37 +0300

    first time

--- 2-Obenglobish/src/Obenglobish.cpp
+++ 2-Obenglobish/src/Obenglobish.cpp
@@ -3,9 +3,61 @@
 #include "console.h"
 #include "simpio.h"
 #include "strlib.h"
+
 using namespace std;
 
+/**
+ * @brief isVowel - function to check, if char is vowel
+ * @param letterToCheck
+ * @return true if vowel
+ */
+bool isVowel(char letterToCheck){
+    char ch=letterToCheck;
+    if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' || ch == 'I' || ch =='o' || ch=='O' ||
+        ch == 'u' || ch == 'U'){
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief obenglobish function translates word to ob-language
+ * @param word to translate
+ * @return translated word as string
+ */
+string obenglobish(string word){
+    string res="";
+
+    bool lastCharIsVowel=false;
+
+    for(int i=0;i<word.length();i++){
+
+        char currentChar=word[i];
+
+        string prefix="";
+
+        if(isVowel(currentChar)){
+            if(!lastCharIsVowel){
+                if(!(((currentChar=='e')||(currentChar=='E'))&&(i==word.length()-1)))
+                    prefix="ob";
+            }
+            lastCharIsVowel=true;
+        }else{
+            lastCharIsVowel=false;
+        }
+
+        res+=prefix+currentChar;
+    }
+    return res;
+}
+
 int main() {
-   // TODO: fill in the code
-   return 0;
+    while(true){
+        string word=getLine("Enter a word");
+        if (word=="")
+            break;
+        string translation=obenglobish(word);
+        cout << word << " -> " << translation << endl;
+    }
+    return 0;
 }
--- 3-NumericConversion/src/NumericConversion.cpp
+++ 3-NumericConversion/src/NumericConversion.cpp
@@ -7,8 +7,49 @@ using namespace std;
 string intToString(int n);
 int stringToInt(string str);
 
+string intToString(int n){
+    if(n<0){
+        return "-"+intToString(-n);
+    }
+    if(n<10){
+        return string()+char(n+'0');
+    }
+    return intToString(n/10)+char(n%10+'0');
+}
+
+
+int stringToInt(string str){
+    if(str[0]=='-')
+        return -stringToInt(str.substr(1,str.length()-1));
+
+    int lastN=char(str[str.length()-1]-'0');
+
+    if(str.length()==1)
+        return lastN;
+
+    return lastN+10*stringToInt(str.substr(0,str.length()-1));
+}
 
 int main() {
-   // TODO: fill in the code
-   return 0;
+    int n=1234;
+    string res=intToString(n);
+    cout<<res<<" "<<res.length()<<endl;
+
+    n=-1234;
+    res=intToString(n);
+    cout<<res<<" "<<res.length()<<endl;
+
+    n=0;
+    res=intToString(n);
+    cout<<res<<" "<<res.length()<<endl;
+
+    string tsts="1234";
+    int res2=stringToInt(tsts);
+    cout<<res2<<endl;
+
+    tsts="-1234";
+    res2=stringToInt(tsts);
+    cout<<res2<<endl;
+
+    return 0;
 }

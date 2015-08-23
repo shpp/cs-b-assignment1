7feab6de4cc9c22550b0865d7375c68c9fa6b530
Dmytro Bohdanov <dmytro.bohdanov@gmail.com>
2015-08-23 17:00:34 +0300

    here are tasks 1-3

--- 1-ConsecutiveHeads/src/ConsecutiveHeads.cpp
+++ 1-ConsecutiveHeads/src/ConsecutiveHeads.cpp
@@ -4,7 +4,47 @@
 #include "random.h"
 using namespace std;
 
+
+
+//heads = true
+//tails = false
+void flipTheCoin();
+
+void doTheGame(int & headsCounter, bool & result, int & flipsCounter);
+
+
 int main() {
-   // TODO: fill in the code
-   return 0;
+    int headsCounter=0;
+    int flipsCounter=0;
+    bool result; //the result of flipping coin
+
+    doTheGame(headsCounter, result, flipsCounter);
+    cout << "It took " << flipsCounter << "flips" << endl;
+    return 0;
+}
+
+
+void flipTheCoin(bool & result){
+    result = rand()%2;
+    //return  x;
+}
+
+void doTheGame(int & headsCounter, bool & result, int & flipsCounter){
+    if(headsCounter==3)
+        return;
+    else{
+        flipTheCoin(result);
+        flipsCounter++;
+        if (result){
+            cout << "heads" << endl;
+            headsCounter++;
+        }
+        else{
+            cout << "tails" << endl;
+            headsCounter = 0;
+        }
+
+        doTheGame(headsCounter, result, flipsCounter);
+    }
+
 }
--- 2-Obenglobish/src/Obenglobish.cpp
+++ 2-Obenglobish/src/Obenglobish.cpp
@@ -5,7 +5,64 @@
 #include "strlib.h"
 using namespace std;
 
+void obenglobish(string word, string & obWord);
+void addLetterToNewWord(char letter, string & obWord, bool vowel);
+
+int NOMBER_OF_LETTERS=6;
+
 int main() {
-   // TODO: fill in the code
+    string obWord=""; //new word
+
+    while (true) {
+        string word = getLine("Enter a word: ");
+        //TODO: rewrite previouse line to  cin>>
+        //cout<<"enter a word";
+        //cin>>word;
+
+        if (word == "") break;
+
+        //string translation = obenglobish(word);
+        obenglobish(word, obWord);
+        cout << word << " -> " << obWord << endl;
+        obWord="";
+    }
+
    return 0;
+}
+
+
+void obenglobish(string word, string & obWord){
+    char letters[NOMBER_OF_LETTERS]={'a','e','y','u','i','o'}; //array of cowels
+//    string obWord=""; //new word
+    bool vowel;
+
+    int temp=word.length();
+
+    for (int i=0; i<=temp-1; i++){
+        vowel=false;
+
+        //cheak if it's the last letter in the word
+        if (i==temp-1){
+            addLetterToNewWord(word[i],obWord, vowel);
+            break;
+        }
+
+        //is the current letter is vowel
+        //if true - add "ob" to new word
+        //but not in case if before this letter is vowel too
+        for (int j=0; j<=NOMBER_OF_LETTERS; j++){
+              if (word[i]==letters[j]){
+                  vowel=true;
+                  break;
+              }
+        }
+        addLetterToNewWord(word[i], obWord, vowel);
+    }
+    //rerurn obWord;
+}
+
+void addLetterToNewWord(char letter, string & obWord, bool vowel){
+    if (vowel)
+        obWord=obWord+"ob";
+    obWord=obWord+letter;
 }

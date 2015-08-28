204906f0defa47d247215418957dc5f799106cc8
alyabihova <sunny4ka@gmail.com>
2015-08-28 17:17:43 +0300

    [4th task done with TokenScanner]

--- 4-Flesch-Kincaid/src/FleschKincaid.cpp
+++ 4-Flesch-Kincaid/src/FleschKincaid.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <string>
 #include "console.h"
 #include "strlib.h"
+#include "tokenscanner.h"
 using namespace std;
 
 
 /*
- * Prompts the user for a filename and tries to open file. In an unsuccessful attempt
+ * Prompts the user for a filename and tries to open file. If an unsuccessful attempt
  * to open a file again prompts the user for a filename. Otherwise returns a filename.
  */
 string requestForFileName(ifstream &);
@@ -20,29 +22,24 @@ string requestForFileName(ifstream &);
 double fleschKincaidGradeLevelTest(ifstream &);
 
 /*
- * Makes calculations of number of syllables, words and sentences in the passed string.
- * @param string takes the line where makes calculations.
- * @params int take a number of words, sentences and syllables in this sequence.
+ * Checks whether an apostrophe is a part of the contraction of words.
+ * @param string takes a token and checks it.
+ * @param TokenScanner allows to read the following token.
+ * @params int takes a number of words by reference for counting.
  */
-void processingLine(string &, int &, int &, int &);
+void checkForContractionOfWords(TokenScanner &, string &, int &);
 
 /*
- * Removes extra spaces between words in passed line.
+ * Makes calculations the approximate number of the syllables in the word.
+ * @param string takes the token in which which counted the syllables.
+ * @params int takes a number of syllables by reference for counting.
  */
-void removeExtraSpaces(string &);
-
-/*
- * Checks: is this a single-character punctuation.
- * @param string takes a line where makes checking passed charecter
- * and also next and previous charecters.
- * @param int takes a possition of a checking character.
- */
-bool isASingleCharacterPunctuation(string &, int);
+void countingSyllables(string &, int &);
 
 /*
  * Checks whether there is a vowel before the letter.
- * @param string takes the line where cheks belonging vowel to one syllable.
- * @param int takes a possition of the letter in the line which is cheked.
+ * @param string takes a token where cheks belonging vowel to one syllable.
+ * @param int takes a possition of the letter in the token which is cheked.
  */
 bool isOneVowel(string &, int);
 
@@ -51,12 +48,22 @@ bool isOneVowel(string &, int);
  */
 bool isVowel(char);
 
+
 /*
- * Checks: is this the end of the sentence or not.
- * @param string takes a line where makes checking passed charecter and next charecter.
- * @param int takes a possition of a checking character.
+ * Checks: is this the end of the sentence or not by checking
+ * whether a token is such punctuation mark which indicate the end of a sentence.
+ * @param string takes a token and checks it.
+ * @param TokenScanner allows to read the following token.
+ * @params int takes a number of sentences by reference for counting.
  */
-bool isSentences(string &, int);
+void countingSentences(TokenScanner &, string &, int &);
+
+/*
+ * Checks whether a token is such punctuation mark which indicate the end of a sentence.
+ * @param string takes a token which is a punctuation mark and
+ * compares it with punctuation marks that indicate the end of a sentence.
+ */
+bool isTheEndOfSentenceChar(string &);
 
 
 /*
@@ -86,65 +93,70 @@ string requestForFileName(ifstream & readFile){
     }
 }
 
+
 double fleschKincaidGradeLevelTest(ifstream & readFile){
     double C_0 = -15.59, C_1 = 0.39, C_2 = 11.8; // constants for calculation the complexity of the text
     int numWords = 0, numSentences = 0, numSyllables = 0;
-    string line = "";
-    while (getline(readFile, line)) {
-        if(line != ""){ // checks for empty string
-            processingLine(line, numWords, numSentences, numSyllables);
+    TokenScanner scanner(readFile); //Initializes a scanner object to read from file by tokens.
+    while (scanner.hasMoreTokens()) {
+        scanner.ignoreWhitespace(); //skip whitespace characters.
+        string token = scanner.nextToken();
+        if(isalpha(token[0])){
+            countingSyllables(token, numSyllables);
+            ++numWords;
+        }
+        if(ispunct(token[0])){
+            checkForContractionOfWords(scanner, token, numWords);
+            countingSentences(scanner, token, numSentences);
         }
     }
     readFile.close();
+
     if (numWords < 1){
         numWords = 1; // to avoid division by zero equate number of words to one
     }
     if (numSentences < 1){
         numSentences = 1; // to avoid division by zero equate number of sentences to one
     }
-    cout << numSyllables << " -> " << numWords << " -> " << numSentences << endl;
+
+    // displays the results counting of syllables, words and sentences
+    cout << "Number of syllables = " << numSyllables << endl;
+    cout << "Number of words = " <<numWords << endl;
+    cout << "Number of sentences = " << numSentences << endl;
+
+    //return the result of calculation complexity of the text by the given formula
     return C_0 + C_1*(((double)numWords)/numSentences) + C_2*(((double)numSyllables)/numWords);
 }
 
 
-void processingLine(string &line, int &numWords, int &numSentences, int &numSyllables){
+void countingSyllables(string &token, int &numSyllables){
     int numSyllablesInWord = 0; //for counting number of syllables in one word
-    removeExtraSpaces(line);
-    line = " " + line; // add one space to the begining of the line for easier counting (to be within the string)
-    for(int i = 1; i < line.length() - 1; ++i){ // loop iterates from the second to the penultimate character, because at the beginning and the end are the spaces
-        if(isspace(line[i])){
-            ++numWords;
-            numSyllablesInWord = 0;
-        }
-        if (isASingleCharacterPunctuation(line, i)){
-             --numWords;
-        }
-        if(isSentences(line, i)){
-            ++numSentences;
-        }
-        if(isOneVowel(line, i)){
-            if((tolower(line[i]) == 'e') && (!isalpha(line[i+1])) && (numSyllablesInWord > 0)){
-                continue;
-            }
-            else {
+    if(isVowel(tolower(token[0]))){ // checks whether the first letter is a vowel
+        ++numSyllables;
+        ++numSyllablesInWord;
+    }
+    if(token.length() > 1){ // if length of the token more than one then check the rest of the charecters
+        for(int i = 1; i < token.length(); ++i){
+            if(isOneVowel(token, i)){
                 ++numSyllables;
                 ++numSyllablesInWord;
             }
         }
-    }
-    ++numWords; // accept that the end of the line is the end of a word too and increase the number of words.
-}
-
-
-void removeExtraSpaces(string &line){
-    line = trim(line) + " "; // add one space to the end of the line for easier checking (to be within the string)
-    for(int i = 0; i < line.length() - 1; ++i){
-        if(isspace(line[i]) && (isspace(line[i+1]))){
-            line.erase(i, 1); //if there are two or more consecutive spaces that remove unwanted one
-            --i; // return the iterator by one ago
+        /*
+         * Reduced the total number of syllables by one in next cases:
+         * - if the last character is a letter 'e' and the previous character is a consonant
+         *   moreover the number of syllables in this token more than one;
+         * - if a token is a part of a contraction of the words, such as "..'ve" or "..'re"
+         */
+        if(((tolower(token[token.length() - 1]) == 'e') && (!isVowel(token[token.length() - 2])) && (numSyllablesInWord > 1))
+                || ((toLowerCase(token) == "re") || (toLowerCase(token) == "ve"))){
+            --numSyllables;
         }
     }
+    return;
 }
+
+
 
 //Checks whether a vowel before another vowel
 bool isOneVowel(string & line, int possition){
@@ -153,7 +165,7 @@ bool isOneVowel(string & line, int possition){
 
 
 bool isVowel(char letter){
-    string vowels = "aeiouy";
+    string vowels = "aeiouy"; // a list of vowels and plus charecter 'y'
     for (char vowel : vowels){
         if (letter == vowel){
             return true;
@@ -163,23 +175,53 @@ bool isVowel(char letter){
 }
 
 
-bool isASingleCharacterPunctuation(string &line, int possition){
-    return ((ispunct(line[possition])) && (isspace(line[possition - 1])) && (isspace(line[possition + 1])));
+void checkForContractionOfWords(TokenScanner & scanner, string &token, int &numWords){
+    /*
+     * If the token is an apostrophe then check whether the next token (if it exists)
+     * is a part of a contraction of the words. To do this, read the next token and
+     * check it for compliance with the following conditions:
+     *  - a token is a single letter and it is a consonant (to exclude the following contraction: "..'s", "..'t", "..'d" and etc);
+     *  - a token is a such contraction of the words as "..'ve", "..'re" or "..'ll".
+     *  if one of these conditions is true then reduce the number of words by one.
+     */
+    if((token == "'") && (scanner.hasMoreTokens())){
+        string nextToken = scanner.nextToken();
+        if(((nextToken.length() == 1) && (tolower(!isVowel(nextToken[0]))))
+                || (toLowerCase(nextToken) == "re")
+                        || (toLowerCase(nextToken) == "ve")
+                                || (toLowerCase(nextToken) == "ll")){
+                --numWords;
+            }
+        scanner.saveToken(nextToken); // read token put back
+    }
 }
 
 
-bool isSentences(string & line, int possition){
-    string punctuationMarks = ".?!"; //punctuation at the end of the sentence
-    for (char mark : punctuationMarks){
-        /*
-         * checks whether a character is on a passed position of punctuation
-         * from a predefined list and checks the next character whether it is a space
-         * or quotes. Thus determine the approximate number of sentences.
-         */
-        if (((line[possition] == mark) && (isspace(line[possition + 1])))
-                || ((line[possition] == mark) && (line[possition + 1] == '"'))){
-            return true;
+void countingSentences(TokenScanner &scanner, string &token, int &numSentences){
+    /*
+     * If the token is one of the punctuation marks which indicate the end of a sentence
+     * then read the next token (if it exists) and check it for the same.
+     * If the next token is not a punctuation mark which indicate the end of a sentence
+     * then increase the number of sentences by one. If passed token was the last one
+     * then also increase the number of sentences by one.
+     */
+    if(isTheEndOfSentenceChar(token)){
+        if(scanner.hasMoreTokens()){
+            string nextToken = scanner.nextToken();
+            if(!isTheEndOfSentenceChar(nextToken)){
+                scanner.saveToken(nextToken);  // read token put back
+                ++numSentences;
+            }
+        }
+        else{
+            ++numSentences;
         }
     }
-    return false;
+}
+
+
+bool isTheEndOfSentenceChar(string & token){
+    string punctuationMarks = ".?!"; //punctuation at the end of the sentence
+    //return true if passed token was found in the list otherwise retutn false
+    return (punctuationMarks.find(token) != string::npos);
 }

c0a6c7851ff2482aac861c4bcff3c73a72ee7c39
ekuryatenko <ekuryatenko@gmail.com>
2015-08-29 15:16:14 +0300

    First commit

--- 1-ConsecutiveHeads/src/ConsecutiveHeads.cpp
+++ 1-ConsecutiveHeads/src/ConsecutiveHeads.cpp
@@ -2,9 +2,66 @@
 #include <string>
 #include "console.h"
 #include "random.h"
+#include <simpio.h>
 using namespace std;
 
+/* Sets console output */
+void consoleSettings();
+/* Lunch proces program */
+void consequtiveHeads();
+/* Main process loop */
+void dropCoin(int heads, int flips);
+/* Sets chances parametr */
+const double chance = 0.5;
+/* Program window title  */
+const string programTitle = "Consequtive heads";
+/* Disable console dupplication */
+const bool consoleEcho = false;
+
 int main() {
-   // TODO: fill in the code
+   /*Adjust console window features*/
+   consoleSettings();
+   /* Programms starts and coins game begins
+    * "heads" getting is monitored, and at 3-d consequtive head programm drops
+    * message and stops  */
+   consequtiveHeads();
+
    return 0;
+}
+/* Lunch game and main counters */
+void consequtiveHeads(){
+    int flipsCnt = 0;
+    int headsCnt = 0;
+    /* Coin is droped until game isn't finished */
+    dropCoin(headsCnt, flipsCnt);
+}
+
+/* Recursion loop
+ * After 3 consequtive heads print end message */
+void dropCoin (int heads, int flips){
+    bool head = false;
+
+    if(heads > 2){
+        /* End game message */
+        cout << "It took " << flips << " flips to get 3 consecutive heads.";
+    }else{
+        /* Coin random result */
+        head = randomChance(chance);
+        flips++;
+
+        if(head){
+            cout << "heads" << endl;
+            heads++;
+        }else{
+            cout << "tails" << endl;
+            heads = 0;
+        }
+        /* Recursion loop */
+        dropCoin(heads, flips);
+    }
+}
+
+void consoleSettings(){
+    setConsoleWindowTitle(programTitle);
+    setConsoleEcho(consoleEcho);
 }
--- 3-NumericConversion/src/NumericConversion.cpp
+++ 3-NumericConversion/src/NumericConversion.cpp
@@ -1,14 +1,114 @@
 #include <iostream>
 #include <string>
 #include "console.h"
+
 using namespace std;
 
-// Function prototypes
+/* Task functions prototypes
+ *  */
 string intToString(int n);
 int stringToInt(string str);
-
+// Service functions prototypes
+string recursiveIntToString(int n, string result);
+int recursiveStrToInt(int index, string str, int result);
+int rise10ToPower(int power);
+// Console settings
+void consoleSettings();
+const double chance = 0.5;
+const string programTitle = "NumericConversion";
+const bool consoleEcho = false;
 
 int main() {
-   // TODO: fill in the code
+   consoleSettings();
+
+   cout << "Process STRING TO INT" << endl;
+   cout << stringToInt("2505") << endl;
+   cout << stringToInt("2") << endl;
+   cout << stringToInt("-252") << endl;
+   cout << stringToInt("0") << endl;   
+
+   cout << endl;
+
+   cout << "Process INT TO STRING" << endl;
+   cout << intToString(-3956) << endl;
+   cout << intToString(-0) << endl;
+   cout << intToString(826) << endl;
+
    return 0;
+}
+
+/* Converts integer to string */
+string intToString(int n){
+    string result = "";
+    if(n < 0){
+        /* Solve negative values */
+        return string("-") + recursiveIntToString(-1 * n, result);
+    }else{
+        /* Main recursive loop */
+        return recursiveIntToString(n, result);
+    }
+}
+
+/* Recursive loop process */
+string recursiveIntToString(int intValue, string result){
+    /* Divide input value in two parts - n/10 and n%10 */
+    int nextValue = intValue/10;
+    if(nextValue == 0){
+        /* Returns string value of integer */
+        return ((char)(intValue + '0') + result);
+    }else{
+        /* finds remain of n%10 process and concantenate it to result  */
+        int remain = intValue % 10;
+        char remainChar = (char)(remain + '0');
+        result = remainChar + result;
+        return recursiveIntToString(nextValue, result);
+    }
+}
+
+/* Converts string to integer
+ * Only if str is not object of integer (no comas or wrong input chars)  */
+int stringToInt(string str){
+    int startIndex = (str.length() - 1);
+    int result = 0;
+    /* Main conversion loop */
+    return recursiveStrToInt(startIndex, str, result);
+}
+
+/* Main conversion process */
+int recursiveStrToInt(int index, string str, int result){
+    if(index < 0){
+        /* If function processed all chars in str */
+        return result;
+    }
+    else{
+        /* If str stores negative value */
+        if((index == 0) && (str[index] == '-')){
+            return -1* result;
+        }else{
+            /* Converts each char into integer */
+            int digit = str[index] - '0';
+            /* Defines decamical capacity of such integer value */
+            int power = (str.length() - 1) - index;
+            /* Sums result with current integer decamical value */
+            result += digit * rise10ToPower(power);
+            /* Prepares to get next char in string */
+            index--;
+            /* Recursive loop */
+            return recursiveStrToInt(index, str, result);
+        }
+    }
+}
+
+/* Rise 10 to power  */
+int rise10ToPower(int power){
+    if(power <= 0){
+        return 1;
+    }else{
+        return 10 * rise10ToPower(power - 1);
+    }
+}
+
+void consoleSettings(){
+    setConsoleWindowTitle(programTitle);
+    setConsoleEcho(consoleEcho);
 }

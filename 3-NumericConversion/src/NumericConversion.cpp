29cf44d7b7ec0d0c1ccd03bbbfd052d13b334ddc
shpp-ekarpenko <elkarpnk@gmail.com>
2015-08-31 00:13:31 +0300

    [Complete]

--- 3-NumericConversion/src/NumericConversion.cpp
+++ 3-NumericConversion/src/NumericConversion.cpp
@@ -7,8 +7,48 @@ using namespace std;
 string intToString(int n);
 int stringToInt(string str);
 
-
 int main() {
-   // TODO: fill in the code
-   return 0;
+    int test1=0;
+    int test2=-89;
+    int test3=2340;
+    cout<<"intToString("<<test1<<") = "<<intToString(test1)<<endl;
+    cout<<"intToString("<<test2<<") = "<<intToString(test2)<<endl;
+    cout<<"intToString("<<test3<<") = "<<intToString(test3)<<endl;
+
+    cout<<"stringToInt("<<test1<<") = "<<stringToInt(test1)<<endl;
+    cout<<"stringToInt("<<test2<<") = "<<stringToInt(test2)<<endl;
+    cout<<"stringToInt("<<test3<<") = "<<stringToInt(test3)<<endl;
+
+    return 0;
+}
+
+/* intToString: This function takes an integer as parameter and returns
+ * its string equivalent
+ */
+string intToString(int n){
+    if(n >= 0){
+        if ( n / 10  == 0 ){
+            return char(n % 10 + '0') + string();
+        } else {
+            return intToString(n / 10) + intToString(n % 10);
+        }
+    }
+    else {
+        return string("-") + intToString(-n);
+    }
+}
+
+/* stringToInt: This function takes a string as parameter and returns
+ * its integer equivalent
+ */
+int stringToInt(string str){
+    if( (str[0] - '0' > 9) ||(str[0] - '0' < 0) ){
+        return 0 - stringToInt(str.substr(1));
+    }else{
+        if(str.length() == 1){
+            return int(str[0] - '0');
+        } else {
+            return stringToInt(str.substr(0,str.length() - 1)) * 10 + stringToInt(str.substr(str.length() - 1));
+        }
+    }
 }
--- 4-Flesch-Kincaid/src/FleschKincaid.cpp
+++ 4-Flesch-Kincaid/src/FleschKincaid.cpp
@@ -1,8 +1,81 @@
 #include <iostream>
+#include <fstream>
+#include <ctype.h>
+#include "tokenscanner.h"
+#include "simpio.h"
 #include "console.h"
 using namespace std;
 
+// Flesch-Kincaid constants
+const double C0 = -15.59;
+const double C1 = 0.39;
+const double C2 = 11.8;
+
 int main() {
-    // TODO: fill in the code
+    ifstream input;
+    string filename;
+    int word = 0, sentence = 0, syllable = 0;
+    string vowels = "aeoiuyAEOIYU";
+
+    while(true){
+        filename = getLine("Enter the filename to be read: ");
+        input.open(filename.c_str());
+        if(input.fail()) {
+            cout << "ERROR: file can't open!" << endl;
+            input.clear();
+        } else break;
+    }
+
+    // Set up tokenscanner
+    TokenScanner scanner(input);
+    scanner.addWordCharacters("'"); // "He's", "I'm" and etc. are tokens
+    scanner.addWordCharacters("-"); // "A-1" is a token
+    scanner.ignoreWhitespace();
+
+    // Scanning using the suggested pattern
+    while (scanner.hasMoreTokens()) {
+        int localSyllable = 0;
+        TokenType type = WORD;
+        string token = scanner.nextToken();
+
+        if (type == scanner.getTokenType(token) && isalpha(token[0])) {
+            word++;
+            if (vowels.find(token[0]) != string::npos) localSyllable++; {
+                for (int i = 1; i < token.length(); i++) {
+                    if (vowels.find(token[i-1]) == string::npos && vowels.find(token[i]) != string::npos) {
+                        localSyllable++;
+                    }
+                }
+                if (token[token.length() - 1] == 'e') {
+                    localSyllable--;
+                }
+                if (localSyllable == 0) {
+                    localSyllable = 1;
+                }
+                syllable += localSyllable;
+            }
+        }
+
+        /* Periods, exclamation points, and question marks
+         * delimit end of sentences
+         */
+        else if (token == "." || token == "!" || token == "?") {
+            sentence++;
+        }
+    }
+
+    if (word == 0) word = 1;
+    if (sentence == 0) sentence = 1;
+
+    /* The resulting number gives an estimate of the grade level
+     * necessary to understand a text
+     */
+    double grade = C0 + C1*(double(word)/sentence) + C2*(double(syllable)/word);
+
+    cout << "Words: " << word << endl;
+    cout << "Sentences: " << sentence << endl;
+    cout << "Syllables: " << syllable << endl;
+    cout << "Grade: " << grade << endl;
+
     return 0;
 }
